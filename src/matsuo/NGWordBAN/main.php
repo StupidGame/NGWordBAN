c1bf9105e23218309bf1ebe676ce01f44a3e0605
matsuo877 <matsuo877@gmail.com>
2020-09-06 15:19:04 +0900

    とりあえずお試しで書きました

--- src/matsuo/NGWordBAN/main.php
+++ src/matsuo/NGWordBAN/main.php
@@ -6,3 +6,11 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class main extends PluginBase implements Listener{
+  public function onEnable(){
+    $this->getLogger()->notice("読み込まれました")
+  }
+
+}
